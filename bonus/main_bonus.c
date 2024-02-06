b3f9e2ab285fcf6a495a6a522ce3d17115b2d798
Dohoon Gwak <dogwak@cx2r8s4.42seoul.kr>
2024-02-06 20:19:28 +0900

    decide flow of the checker program.

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -6,21 +6,18 @@
 /*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:56:14 by dogwak            #+#    #+#             */
-/*   Updated: 2024/02/01 19:06:26 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/02/06 20:10:28 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../libft/libft.h"
 #include "../ft_string/ft_string.h"
 #include "stack_ab_bonus.h"
 
-int	main(int argc, char *argv[])
+void	check_push_swap(/*two stack ptr*/)
 {
-	// input check
 
-	// parse_argv
-	// get data from argv and convert to int, put them in the deque a
-
-	while (true)
+	while (1)
 	{
 		// getline, read till eof
 		if ()
@@ -37,4 +34,45 @@ int	main(int argc, char *argv[])
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
+
+}
+
+void	delete_strs(char **strs)
+{
+	int		idx;
+
+	idx = -1;
+	while (strs[++idx] != NULL)
+		free(strs[idx]);
+	free(strs);
+}
+
+int	main(int argc, char *argv[])
+{
+	char		**tokens;
+	t_stack_ab	*pab;
+	// two stack
+
+	if (argc == 1)
+	{
+		write(2, "Error\n", 6);
+		exit(1);
+	}
+	if (argc == 2)
+	{
+		tokens = ft_split(argv[1], ' ');
+		// construct two stack
+		delete_strs(tokens);
+	}
+	else
+	{
+		tokens = argv + 1;
+		// construct two stack
+	}
+	if (pab == NULL)	// stack construct failed.
+	{
+		write(2, "Error\n", 6);
+		exit(1);
+	}
+	// check_push_swap(two stack);
 }
