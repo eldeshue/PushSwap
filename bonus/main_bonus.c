a6bb03376171c507c32714d2052f0304f58bc0c8
Dohoon Gwak <dogwak@cx2r8s4.42seoul.kr>
2024-02-07 20:03:33 +0900

    implement main. error handling needed.

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -6,35 +6,35 @@
 /*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:56:14 by dogwak            #+#    #+#             */
-/*   Updated: 2024/02/06 20:10:28 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/02/07 19:53:23 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "../ft_string/ft_string.h"
 #include "stack_ab_bonus.h"
+#include "push_swap_bonus.h"
 
-void	check_push_swap(/*two stack ptr*/)
+void	check_push_swap(t_stack_ab *pab)
 {
+	t_ft_string	buffer;
 
+	buffer = construct_ftstr();
 	while (1)
 	{
-		// getline, read till eof
-		if ()
+		buffer.getline(&buffer, 0);
+		if (buffer.size == 0)
 			break ;
-		// input sanity check
-
-		// compare and get command from input
-
-		// manipulate stack
-
+		if (!select_query(pab, buffer.c_str(&buffer)))
+		{
+			// error handling
+		}
 	}
-	// check stack a all ordered and b empty
-	if ()
+	destruct_ftstr(&buffer);
+	if (is_ab_sorted(pab))
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
-
 }
 
 void	delete_strs(char **strs)
@@ -51,7 +51,6 @@ int	main(int argc, char *argv[])
 {
 	char		**tokens;
 	t_stack_ab	*pab;
-	// two stack
 
 	if (argc == 1)
 	{
@@ -61,18 +60,18 @@ int	main(int argc, char *argv[])
 	if (argc == 2)
 	{
 		tokens = ft_split(argv[1], ' ');
-		// construct two stack
+		pab = new_ab(tokens);
 		delete_strs(tokens);
 	}
 	else
 	{
 		tokens = argv + 1;
-		// construct two stack
+		pab = new_ab(tokens);
 	}
-	if (pab == NULL)	// stack construct failed.
+	if (pab == NULL)
 	{
 		write(2, "Error\n", 6);
 		exit(1);
 	}
-	// check_push_swap(two stack);
+	check_push_swap(pab);
 }
