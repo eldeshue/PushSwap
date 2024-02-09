e4b7d50346c091c3c8aaa3db5282e14219fb32f7
iphuck22 <eldeshue@naver.com>
2024-02-09 16:43:00 +0900

    fix, init field of stack_ab

--- bonus/construct_ab_bonus.c
+++ bonus/construct_ab_bonus.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   construct_ab_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dogwak <dogwak@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 20:01:48 by dogwak            #+#    #+#             */
-/*   Updated: 2024/02/07 15:59:24 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/02/08 19:37:31 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "stack_ab_bonus.h"
 #include "push_swap_parser_bonus.h"
+#include "stack_ab_bonus.h"
+#include <stdlib.h>
 
 // construct ab stack.
 // parser module needed
@@ -36,6 +36,10 @@ t_stack_ab	*new_ab(char **strs)
 		this->pdata[idx] = ft_atoi(strs[str_cnt - 1 - idx]);
 	if (!normailize_int(this->pdata, this->size))
 		return (NULL);
+	this->a_bot = 0;
+	this->b_bot = str_cnt;
+	this->pivot = str_cnt - 1;
+	this->size = str_cnt;
 	return (this);
 }
 
