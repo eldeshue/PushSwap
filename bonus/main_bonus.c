e147149b859c036f48b801105149cd70b6867d87
iphuck22 <eldeshue@naver.com>
2024-02-13 16:17:46 +0900

    fix error, no argument.

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -6,25 +6,25 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:56:14 by dogwak            #+#    #+#             */
-/*   Updated: 2024/02/09 16:54:18 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/02/13 16:03:15 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
 #include "../ft_string/ft_string.h"
-#include "stack_ab_bonus.h"
+#include "../libft/libft.h"
 #include "push_swap_bonus.h"
+#include "stack_ab_bonus.h"
 
-void check_push_swap(t_stack_ab *pab)
+void	check_push_swap(t_stack_ab *pab)
 {
-	t_ft_string buffer;
+	t_ft_string	buffer;
 
 	buffer = construct_ftstr();
 	while (1)
 	{
 		buffer.getline(&buffer, 0);
 		if (buffer.size == 0)
-			break;
+			break ;
 		if (!select_query(pab, buffer.c_str(&buffer), buffer.size))
 		{
 			write(2, "Error\n", 6);
@@ -38,9 +38,9 @@ void check_push_swap(t_stack_ab *pab)
 		write(1, "KO\n", 3);
 }
 
-void delete_strs(char **strs)
+void	delete_strs(char **strs)
 {
-	int idx;
+	int	idx;
 
 	idx = -1;
 	while (strs[++idx] != NULL)
@@ -48,16 +48,13 @@ void delete_strs(char **strs)
 	free(strs);
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	char **tokens;
-	t_stack_ab *pab;
+	char		**tokens;
+	t_stack_ab	*pab;
 
 	if (argc == 1)
-	{
-		write(2, "Error\n", 6);
 		exit(1);
-	}
 	if (argc == 2)
 	{
 		tokens = ft_split(argv[1], ' ');
@@ -75,4 +72,5 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	check_push_swap(pab);
+	delete_ab(pab);
 }
